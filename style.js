dedd27eaf988d785ceeb0d68f19c7e770b60ecde
loknathsd <loknathsds66@gmail.com>
2021-02-08 13:13:14 +0600

    almost complete

--- style.js
+++ style.js
@@ -1,44 +1,13 @@
 
-//  const url =`https://www.themealdb.com/api/json/v1/1/search.php?f=${input}`
-//  fetch(url)
-// .then(res => res.json())
-// .then(data => displayMeals(data.meals));
-
-
-
-// const displayMeals = foods => {
-//     const mealsDiv = document.getElementById("meals-area");
-//     let input = document.getElementById("searchInput").value;
-//     console.log(input);
-// for (let i = 0; i < foods.length; i++) {
-//     const mealName = foods[i];
-
-//     const div = document.createElement("div");
-//     div.className = "meal-area";
-
-//     const mealInfo = `
-//     <img class="img-size" src ="${mealName.strMealThumb}">
-//     <h3 class="name-title">${mealName.strMeal}</h3>
-// `;
-//     div.innerHTML = mealInfo;
-
-//     mealsDiv.appendChild(div);
-
-
-// }
-
-//     };
 const mealInfo = document.getElementById("mealInfo");
 const mealList = document.getElementById("meals-area");
 const button = document.getElementById("searchButton");
 button.addEventListener("click", getMeal);
-// mealList.addEventListener("click", function () {
-//     console.log("me too clicked");
-// });
+
 
 function getMeal() {
     let searchInputText = document.getElementById("searchInput").value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputText}`)
         .then(response => response.json())
         .then(data => {
             let html = "";
@@ -48,6 +17,7 @@ function getMeal() {
                <div class="meal-area">
                <img class="img-size" src ="${meal.strMealThumb}">
                <h3 class="name-title">${meal.strMeal}</h3>
+               <button class="detail-btn" onclick = "getMealInfo('${meal.idMeal}')">Detail</button>
                </div>
                `
                 });
@@ -57,9 +27,41 @@ function getMeal() {
         });
 
 };
-////////
 
 
+const getMealInfo = id => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then(res =>res.json())
+    .then(data =>{
+        let html = "";
+        if (data.meals) {
+            data.meals.forEach(meal => {
+                html += `
+           <div class="mealInfo-area">
+           <img class="info-img-size" src ="${meal.strMealThumb}">
+           <h2 class="name-title">${meal.strMeal}</h2>
+           <p>Ingredients</p>
+                    <ul>                    
+                        <li>${meal.strIngredient1}</li>
+                        <li>${meal.strIngredient2}</li>
+                        <li>${meal.strIngredient3}</li>
+                        <li>${meal.strIngredient4}</li>
+                        <li>${meal.strIngredient5}</li>
+                        <li>${meal.strIngredient6}</li>
+                        <li>${meal.strIngredient7}</li>
+                        <li>${meal.strIngredient8}</li>
+                        <li>${meal.strIngredient9}</li>
+                        <li>${meal.strIngredient10}</li>
+                    </ul>
+           
+           <div>
+           `
+            });
+        }
+        mealInfo.innerHTML = html;
+
+})
+};
 
 
 
